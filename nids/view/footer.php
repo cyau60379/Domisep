fcc24b41e3f126aaa23d758fdc5c92bb432912a1
Barty974 <48982280+Barty974@users.noreply.github.com>
2019-04-02 14:28:33 +0200

    Add files via upload
    Rajout Footer

--- nids/view/footer.php
+++ nids/view/footer.php
@@ -1,1 +1,110 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: basti
+ * Date: 25/03/2019
+ * Time: 11:49
+ */
+
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="/design/styleFooter.css">
+
+<html>
+<body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<footer>
+    <div class="liste1">
+        <img src="/Images/logoNids.png" alt="notre logo"><br>© 2019 Copyright: NIDS
+    </div>
+    <div class="liste">
+        <li>
+            <a href="#!">Support</a>
+        </li>
+        <li>
+            <a href="#!">FAQ</a>
+        </li>
+        <li>
+            <a href="#!">Catalogue</a>
+        </li>
+        <br>
+    </div>
+
+    <div class="liste">
+        <li>
+            Contact :
+        </li>
+        <li>
+            <a href="#!">10 Rue de Vanves
+                <br>92130 Issy-Les-Moulineaux</a>
+        </li>
+        <li>
+            09XXXXXXXX
+        </li>
+        <br>
+    </div>
+</footer>
+</body>
+</html>
