a3fe4e114fd032cd2582b23713c8778ded393d32
julien <julien.parizot@isep.fr>
2019-04-12 16:29:26 +0200

    modèle temporaire

--- nids/model/requetesUtilisateur.php
+++ nids/model/requetesUtilisateur.php
@@ -1,1 +1,22 @@
 <?php
+
+include($_SERVER["DOCUMENT_ROOT"] . "/model/connexionBDD.php");
+
+
+function ajouterInscription(PDO $bdd, $valNom, $valPrenom, $valAdresse_mail, $valDate, $valMdp): array
+{
+    $valMdp = password_hash($valMdp, $id, PASSWORD_DEFAULT);
+    $query = 'INSERT INTO utilisateur (Nom, Prenom, Adresse_mail, Date_naissance, Mot_de_passe) VALUES ($valNom, $valPrenom, $valAdresse_mail, $valDate, $valMdp )' . $id; //rajoute les données du nouvel utilisateur
+    $bdd->exec($query);
+
+}
+
+function adresse_mailMdp(PDO $bdd, $valAdresse_mail)
+{
+    $query = 'SELECT utilisateur.id FROM utilisateur WHERE utilisateur.Adresse_mail="$valAdresse_mail"' . $valAdresse_mail;
+    $table = $bdd->query($query)->fetchAll();
+    $id = $table[0];
+    $query = 'SELECT utilisateur.Mot_de_passe FROM utilisateur WHERE utilisateur.Adresse_mail="$valAdresse_mail"' . $id;
+    $table = $bdd->query($query);
+    return $table[0];  //renvoie le Mot de passe
+}
